120c0061a91faf4ae5952a253408fbe7a4c2ee7b
jclas7 <jclas@naver.com>
2026-01-12 18:19:48 +0900

    20260112-백앤드-JWT

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,37 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieParse = require("cookie-parser");
+const cors = require("cors");
 const app = express();
 const PORT = 3000;
+
+const userRoutes = require("./routes/user");
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cookieParse());
+
+app.use("/api/auth", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("헬로 월드~~");
 });
 
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("몽고db와 연결"))
+  .catch((error) => console.log("연결실패:", error));
+
+console.log(".. 리스닝...");
+
 app.listen(PORT, () => {
-  console.log("서버 리스닝...");
+  console.log("서버 리스닝...2");
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,49 @@ import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import { BrowserRouter } from "react-router-dom";
-import { Outlet, createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  Outlet,
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
 import MainPage from "./pages/mainPage/MainPage";
 import About from "./pages/about/About";
 import Leadership from "./pages/leadership/Leadership";
 import Contact from "./pages/contact/Contact";
 import Board from "./pages/board/Board";
 import Services from "./pages/services/Services";
+
+import AdminLogin from "./pages/admin/AdminLogin";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function AuthRedirectRoute() {
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
+
+  useEffect(() => {
+    const verifyToken = async () => {
+      try {
+        const response = await axios.post(
+          "http://localhost:3000/api/auth/verify-token",
+          {},
+          { withCredentials: true }
+        );
+        setIsAuthenticated(true);
+      } catch (error) {
+        console.log("토큰 인증 실패: ", error);
+        setIsAuthenticated(false);
+      }
+    };
+    verifyToken();
+  }, []);
+
+  if (isAuthenticated === null) {
+    return null;
+  }
+
+  return isAuthenticated ? <Navigate to="/admin/posts" replace /> : <Outlet />;
+}
 
 function Layout() {
   return (
@@ -32,6 +68,11 @@ const router = createBrowserRouter([
       { path: "/contact", element: <Contact /> },
       { path: "/our-service", element: <Services /> },
     ],
+  },
+  {
+    path: "/admin",
+    element: <AuthRedirectRoute />,
+    children: [{ index: true, element: <AdminLogin /> }],
   },
 ]);
 
